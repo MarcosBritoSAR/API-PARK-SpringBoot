4c25149323534c42b131ee8c6e5ab7deb079d021
Marcos Brito <marcosbryto92@gmail.com>
2024-12-09 15:26:20 -0300

    Criando o metodo timezeneConfig.
    Isso garante que apos a inicializacao da classe pelo Spring, o metodo construtor dela será inicializado e em seguinda o proximo metodo a ser executado será o timezene config

--- src/main/java/com/marcosbrito/parkapi/config/SpringTimezoneConfig.java
+++ src/main/java/com/marcosbrito/parkapi/config/SpringTimezoneConfig.java
@@ -1,7 +1,16 @@
 package com.marcosbrito.parkapi.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
 
 @Configuration
 public class SpringTimezoneConfig {
+
+    @PostConstruct
+    public void timenoneConfig(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+
 }
