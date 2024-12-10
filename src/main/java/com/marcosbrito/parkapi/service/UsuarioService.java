fd7ecb836f0f851a02c446f25925ad9ad2742a70
Marcos Brito <marcosbryto92@gmail.com>
2024-12-10 14:44:10 -0300

    Aplicando de fato injecao de dependencia pelo construtor usando lombok

--- src/main/java/com/marcosbrito/parkapi/service/UsuarioService.java
+++ src/main/java/com/marcosbrito/parkapi/service/UsuarioService.java
@@ -1,10 +1,19 @@
 package com.marcosbrito.parkapi.service;
 
 
+import com.marcosbrito.parkapi.repository.UsuaruioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+/*
+* O usar  @RequiredArgsConstructor, o lombok usa o construtor pra injetar a dependencia
+*
+* */
 public class UsuarioService {
+
+    private final UsuaruioRepository usuaruioRepository; //para que o lambok, é importante que eu 
+    //eu declares as minhas dependencias como final. Interessante né? 
+
 }
