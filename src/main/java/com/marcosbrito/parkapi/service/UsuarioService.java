f81aa70a6ea02c12af1b921c165ffae59fa1c6a1
Marcos Brito <marcosbryto92@gmail.com>
2024-12-10 17:21:06 -0300

    Implementando Buscar por usuario

--- src/main/java/com/marcosbrito/parkapi/service/UsuarioService.java
+++ src/main/java/com/marcosbrito/parkapi/service/UsuarioService.java
@@ -4,8 +4,11 @@ package com.marcosbrito.parkapi.service;
 import com.marcosbrito.parkapi.entity.Usuario;
 import com.marcosbrito.parkapi.repository.UsuaruioRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 
@@ -21,6 +24,11 @@ public class UsuarioService {
     @Transactional
     public Usuario salvar(Usuario usuario) {
         return usuaruioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true) //Essa anotation denuncia pro spring que esse metodo é exclusivo para consulta no DB
+    public Usuario getUsuario(Long id) {
+        return usuaruioRepository.findById(id).orElseThrow(() -> new RuntimeException("Invalid id: " + id));
     }
     //eu declares as minhas dependencias como final. Interessante né?
 
--- src/main/java/com/marcosbrito/parkapi/web/controller/UsuarioController.java
+++ src/main/java/com/marcosbrito/parkapi/web/controller/UsuarioController.java
@@ -3,11 +3,9 @@ package com.marcosbrito.parkapi.web.controller;
 import com.marcosbrito.parkapi.entity.Usuario;
 import com.marcosbrito.parkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.Option;
 import java.util.List;
@@ -24,6 +22,13 @@ public class UsuarioController {
         Usuario usuario1 = usuarioService.salvar(usuario);
         // Retornando uma resposta para o cliente
         return ResponseEntity.ok().body(usuario1);
+    }
+
+    //Ao par de chave denuncia que o objetivo é receber um valor da URL e usar como parametro
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getUsusarioById(@PathVariable Long id) {
+            Usuario user = usuarioService.getUsuario(id);
+            return ResponseEntity.ok().body(user);
     }
 
     /*
