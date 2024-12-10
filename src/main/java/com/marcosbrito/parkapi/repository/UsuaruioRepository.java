24acebbec7f75196a83c51f58af22ef397f8beca
Marcos Brito <marcosbryto92@gmail.com>
2024-12-10 14:40:00 -0300

    Criando os servieces e os repositorys do ususario e usando a anotacao lombok pra injecao de dependencia

---
+++ src/main/java/com/marcosbrito/parkapi/repository/UsuaruioRepository.java
@@ -0,0 +1,8 @@
+package com.marcosbrito.parkapi.repository;
+
+import com.marcosbrito.parkapi.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuaruioRepository extends JpaRepository<Usuario, Long> {
+
+}
---
+++ src/main/java/com/marcosbrito/parkapi/service/UsuarioService.java
@@ -0,0 +1,10 @@
+package com.marcosbrito.parkapi.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+}
