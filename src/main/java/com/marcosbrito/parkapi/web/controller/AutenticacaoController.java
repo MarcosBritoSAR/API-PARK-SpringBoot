203caf987ac778e81be124c189862add8c389ddf
Marcos Brito <marcosbryto92@gmail.com>
2024-12-13 12:03:28 -0300

    Criando o endPoint para a autentificacao - O controller de Autenticação

---
+++ src/main/java/com/marcosbrito/parkapi/web/controller/AutenticacaoController.java
@@ -0,0 +1,69 @@
+package com.marcosbrito.parkapi.web.controller;
+
+//Criando um endPoint para a autenticacaoController
+
+import com.marcosbrito.parkapi.jwt.JwtToken;
+import com.marcosbrito.parkapi.jwt.JwtUserDetailsService;
+import com.marcosbrito.parkapi.web.dto.UsuarioLoginDto;
+import com.marcosbrito.parkapi.web.exception.ErrorMessage;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class AutenticacaoController {
+
+    public final JwtUserDetailsService jwtUserDetailsService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/auth")
+    // Metodo para autenticar o usuário utilizando as credenciais fornecidas.
+    public ResponseEntity<?> autenticar(@RequestBody @Valid UsuarioLoginDto usuarioLoginDto, HttpServletRequest request) {
+        // Loga o início do processo de autenticação, incluindo as informações do DTO recebido.
+        log.info("Processo de autenticacao pelo login{}", usuarioLoginDto.toString());
+
+        try {
+            // Cria um objeto de autenticação contendo o nome de usuário e senha do DTO.
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken( //Verifica se existe um usiario som essa senha - Se nao existir, lanca uma exception
+                            usuarioLoginDto.getUsername(), // Nome de usuário fornecido
+                            usuarioLoginDto.getPassword()  // Senha fornecida
+                    );
+
+            // Tenta autenticar o usuário utilizando o gerenciador de autenticação.
+            authenticationManager.authenticate(authenticationToken);
+
+            // Gera um token JWT para o usuário autenticado, utilizando o serviço de detalhes do usuário.
+            JwtToken jwtToken = jwtUserDetailsService.getTokenAuthenticated(usuarioLoginDto.getUsername());
+
+            // Retorna uma resposta HTTP 200 (OK) contendo o token JWT gerado.
+            return ResponseEntity.ok(jwtToken);
+        } catch (AuthenticationException e) {
+            // Loga uma mensagem de aviso caso ocorra uma falha na autenticação.
+            log.warn("Falha no processo de autenticacao pelo login{}", usuarioLoginDto.toString());
+        }
+
+        // Caso a autenticação falhe, retorna uma resposta HTTP 400 (BAD REQUEST)
+        // com uma mensagem de erro personalizada.
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorMessage(
+                        request, // O objeto da requisição original
+                        HttpStatus.BAD_REQUEST, // Código HTTP 400
+                        "Credenciais invalidas" // Mensagem de erro retornada ao cliente
+                ));
+    }
+}
