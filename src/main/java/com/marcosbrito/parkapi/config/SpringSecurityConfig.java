9e9314c9048f8f821fa318f84ab00a74bcded836
Marcos Brito <marcosbryto92@gmail.com>
2024-12-13 09:48:25 -0300

    Implementando JWT no projeto - Configurations

---
+++ src/main/java/com/marcosbrito/parkapi/config/SpringSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.marcosbrito.parkapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebMvc
+
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //A partir desse retorno, eu defino todos as operacoes do filtro
+
+        /*
+        Como estamos trabalhando com spring security, precisamos desabilitar algumas coisas
+        */
+
+        return http
+                .csrf(scrf -> scrf.disable())
+                .formLogin(form -> form.disable())//Desabilitando formulario de login
+                .httpBasic(basic -> basic.disable())//Desabilitando a autenticação do spring security
+                .authorizeHttpRequests(auth-> auth//incluindo um bloco de autoricao
+                        .requestMatchers(HttpMethod.POST, "api/v1/usuarios").permitAll() //Autorizo a qualquer usuario acessar usuarios e usar o metodo post para criar um novo usuario
+                        .anyRequest().authenticated() //Com excecao do metodo acima, todos os outros precisaram ter acesso para realizar operacoes
+                ).sessionManagement(
+                    session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)//Politica da sessão é do tipo stateless
+                ).build();
+    }
+
+
+    @Bean //Configurando o tipo de criptografia
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); //Uma das criptografias mais seguras
+    }
+
+    @Bean //Referente ao gerencimento de autenticacao
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
