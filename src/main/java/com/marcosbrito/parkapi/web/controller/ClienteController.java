1d726e296e3b1d61604ec8df3732aa000e19c546
Marcos Brito <marcosbryto92@gmail.com>
2024-12-17 00:23:05 -0300

    Recurso para localizar um cliente pelo identificador

--- src/main/java/com/marcosbrito/parkapi/web/controller/ClienteController.java
+++ src/main/java/com/marcosbrito/parkapi/web/controller/ClienteController.java
@@ -8,6 +8,12 @@ import com.marcosbrito.parkapi.service.UsuarioService;
 import com.marcosbrito.parkapi.web.dto.ClienteCreateDto;
 import com.marcosbrito.parkapi.web.dto.ClienteResponseDto;
 import com.marcosbrito.parkapi.web.dto.mapper.ClienteMapper;
+import com.marcosbrito.parkapi.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -41,8 +47,19 @@ public class ClienteController {
 
 
     //preciso de um recurso que retornar um cliente pelo id
-
+    @Operation(summary = "Localizar um cliente", description = "Recurso para localizar um cliente pelo ID. " +
+            "Requisição exige uso de um bearer token. Acesso restrito a Role='ADMIN'",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso localizado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ClienteResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Cliente não encontrado",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de CLIENTE",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ClienteResponseDto> getById(@PathVariable @Valid Long id) {
         Cliente cliente = clienteService.buscarPorId(id);
         return ResponseEntity.ok().body(ClienteMapper.toDto(cliente));
